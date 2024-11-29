86a3c7780ed44c55d1a0133c1469a3e3ff5c2586
Harshkesharwani789 <atul123564@gmail.com>
2024-11-30 00:35:22 +0530

    init

---
+++ src/components/breadcrum/Breadcrum.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import "./Breadcrum.css";
+import { useLocation, Link } from "react-router-dom";
+
+function Breadcrum() {
+  const path = useLocation().pathname;
+  const [isResultPage, setIsResultPage] = useState(false);
+
+  useEffect(() => {
+    if (path === "/result") {
+      setIsResultPage(true);
+    }
+  }, [path]);
+
+  return (
+    <>
+      <div className="breadcrum-container hidden sm:flex">
+        <Link to="/" className="breadcrumb-link">Home</Link>
+        <span>/</span>
+        <span className="breadcrumb-text">IVF Success Rate Calculator</span>
+        {isResultPage && (
+          <>
+            <span>/</span>
+            <span>Result</span>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Breadcrum;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logos/ivf-pulse-logo.png";
+import "./Header.css";
+import Hamburger from "hamburger-react";
+import FormModal from "../formModal/FormModal";
+
+function Header() {
+  const [isOpen, setOpen] = useState(false);
+  const [isFormOpen, setFormOpen] = useState(false);
+
+  const openForm = () => {
+    setFormOpen(true);
+  };
+
+  return (
+    <>
+      <div className="header-container">
+        <div>
+          <img src={logo} alt="IVF Pulse Logo" />
+        </div>
+        <div className="nav-links">
+          <Link>Donor Programme</Link>
+          <Link>Fertility Preservation</Link>
+          <Link>Advanced Treatments</Link>
+          <Link>Infertility Treatments</Link>
+          <Link>IVF Testing</Link>
+          <Link>About Us</Link>
+          <button className="talk-button" onClick={openForm}>
+            Talk to Us <span className="arrow">→</span>
+          </button>
+        </div>
+        <div className="hamburger">
+          <Hamburger toggled={isOpen} toggle={setOpen} />
+        </div>
+      </div>
+      {isOpen && (
+        <div className="ham-nav-links">
+          <Link>Donor Programme</Link>
+          <Link>Fertility Preservation</Link>
+          <Link>Advanced Treatments</Link>
+          <Link>Infertility Treatments</Link>
+          <Link>IVF Testing</Link>
+          <Link>About Us</Link>
+          <button onClick={openForm}>Talk to Us</button>
+        </div>
+      )}
+      {isFormOpen && <FormModal closeForm={() => setFormOpen(false)} />}
+    </>
+  );
+}
+
+export default Header;
